ee6bcab64258fd0cdd33739b8e7d092811904c89
zsiyed <69491969+zsiyed@users.noreply.github.com>
2022-02-04 14:13:10 -0800

    add lib and code

---
+++ SkillDemoTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+public class SkillDemoTest {
+    @Test
+    public void multiply() {
+        assertEquals(1, SkillDemo.multiply(2)); // should fail
+    }
+}
