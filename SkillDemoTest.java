37f579e10982f41fade7b865ac36c1ee11a71589
zsiyed <69491969+zsiyed@users.noreply.github.com>
2022-02-04 14:14:57 -0800

    fixed multiply

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -3,6 +3,6 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void multiply() {
-        assertEquals(1, SkillDemo.multiply(2)); // should fail
+        assertEquals(4, SkillDemo.multiply(2)); // should fail
     }
 }
